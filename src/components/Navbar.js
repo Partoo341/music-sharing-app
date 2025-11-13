cb61081f81892ac4e3f76cdd284c55e6318319b0
PartoOrganist <patrickirungumithamo@gmail.com>
2025-11-13 18:30:57 +0300

    Fix ESLint errors and BOM character

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAudio } from '../context/AudioContext';
 import './Navbar.css';
 
